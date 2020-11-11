81da8ec9d3014518cc23f366d3ffb558b1b5490d
DanielDVD <dan.vargasdiaz@gmail.com>
2020-11-10 21:18:38 -0600

    Module

---
+++ intFSM/Module.v
@@ -0,0 +1,95 @@
+`include "Main.v"
+`include "VC0.v"
+`include "VC1.v"
+`include "D0.v"
+`include "D1.v"
+`include "fifo_main_pop.v"
+`include "pop_delay_vc0.v"
+`include "demux_dest.v"
+`include "demux_vc_id.v"
+`include "mux.v"
+`include "fifo.v"
+`include "fifo16.v"
+`include "maquina_estados_cond"
+`include "interconnect"
+
+module Module (    
+    input clk,
+    input reset_L,
+    input Main_wr,
+    input [(BW-1):0] Main_data_in,
+    input D0_rd,
+    input D1_rd,
+    output [(BW-1):0] D0_data_out,
+    output [(BW-1):0] D1_data_out,
+    input init,
+    //MF
+    input [3:0] UmbralesMFs_HIGH,
+    input [3:0] UmbralesMFs_LOW,
+    //VF
+    input [31:0] UmbralesVCs_HIGH,
+    input [31:0] UmbralesVCs_LOW,
+    //DF
+    input [7:0] UmbralesDs_HIGH,
+    input [7:0] UmbralesDs_LOW,
+    output reg error_out_cond,
+    output reg active_out_cond,
+    output reg idle_out_cond,
+    output reg [4:0] error_full_cond);
+    
+    parameter BW=6;
+
+interconnect intern0(/*AUTOINST*/
+		     // Outputs
+		     .D0_data_out	(D0_data_out[(BW-1):0]),
+		     .D1_data_out	(D1_data_out[(BW-1):0]),
+		     .Main_error_output	(Main_error_output),
+		     .Main_empty	(Main_empty),
+		     .Main_full		(Main_full),
+		     .VC0_error_output	(VC0_error_output),
+		     .VC0_empty		(VC0_empty),
+		     .VC1_error_output	(VC1_error_output),
+		     .VC1_empty		(VC1_empty),
+		     .D0_error_output	(D0_error_output),
+		     .D0_empty		(D0_empty),
+		     .D1_error_output	(D1_error_output),
+		     .D1_empty		(D1_empty),
+		     // Inputs
+		     .clk		(clk),
+		     .reset_L		(reset_L),
+		     .Main_wr		(Main_wr),
+		     .Main_data_in	(Main_data_in[(BW-1):0]),
+		     .D0_rd		(D0_rd),
+		     .D1_rd		(D1_rd));
+
+maquina_estados_cond maquina(/*AUTOINST*/
+			     // Outputs
+			     .error_out_cond	(error_out_cond),
+			     .active_out_cond	(active_out_cond),
+			     .idle_out_cond	(idle_out_cond),
+			     .UmbralMF_HIGH_cond(UmbralMF_HIGH_cond[3:0]),
+			     .UmbralMF_LOW_cond	(UmbralMF_LOW_cond[3:0]),
+			     .UmbralV0_HIGH_cond(UmbralV0_HIGH_cond[15:0]),
+			     .UmbralV0_LOW_cond	(UmbralV0_LOW_cond[15:0]),
+			     .UmbralV1_HIGH_cond(UmbralV1_HIGH_cond[15:0]),
+			     .UmbralV1_LOW_cond	(UmbralV1_LOW_cond[15:0]),
+			     .UmbralD0_HIGH_cond(UmbralD0_HIGH_cond[3:0]),
+			     .UmbralD0_LOW_cond	(UmbralD0_LOW_cond[3:0]),
+			     .UmbralD1_HIGH_cond(UmbralD1_HIGH_cond[3:0]),
+			     .UmbralD1_LOW_cond	(UmbralD1_LOW_cond[3:0]),
+			     .error_full_cond	(error_full_cond[4:0]),
+			     // Inputs
+			     .clk		(clk),
+			     .init		(init),
+			     .UmbralesMFs_HIGH	(UmbralesMFs_HIGH[3:0]),
+			     .UmbralesMFs_LOW	(UmbralesMFs_LOW[3:0]),
+			     .UmbralesVCs_HIGH	(UmbralesVCs_HIGH[31:0]),
+			     .UmbralesVCs_LOW	(UmbralesVCs_LOW[31:0]),
+			     .UmbralesDs_HIGH	(UmbralesDs_HIGH[7:0]),
+			     .UmbralesDs_LOW	(UmbralesDs_LOW[7:0]),
+			     .reset_L		(reset_L),
+			     .FIFO_EMPTIES	(FIFO_EMPTIES[4:0]),
+			     .FIFO_ERRORS	(FIFO_ERRORS[4:0]));
+
+
+endmodule
--- intFSM/maquina_estados_cond.v
+++ intFSM/maquina_estados_cond.v
@@ -2,21 +2,35 @@ module maquina_estados_cond
    (//ENTRADAS   
     input clk,
     input init,
-    input [3:0] UmbralesMFs,
-    input [31:0] UmbralesVCs,
-    input [7:0] UmbralesDs,
-	input reset,
+    //MF
+    input [3:0] UmbralesMFs_HIGH,
+    input [3:0] UmbralesMFs_LOW,
+    //VF
+    input [31:0] UmbralesVCs_HIGH,
+    input [31:0] UmbralesVCs_LOW,
+    //DF
+    input [7:0] UmbralesDs_HIGH,
+    input [7:0] UmbralesDs_LOW,
+	input reset_L,
 	input [4:0] FIFO_EMPTIES,
     input [4:0] FIFO_ERRORS,
     //SALIDAS
 	output reg error_out_cond,
     output reg active_out_cond,
     output reg idle_out_cond,
-    output reg [3:0] UmbralMF_cond,
-    output reg [15:0] UmbralV0_cond,
-    output reg [15:0] UmbralV1_cond,
-    output reg [3:0] UmbralD0_cond,
-    output reg [3:0] UmbralD1_cond,
+    //MF
+    output reg [3:0] UmbralMF_HIGH_cond,
+    output reg [3:0] UmbralMF_LOW_cond,
+    //VF
+    output reg [15:0] UmbralV0_HIGH_cond,
+    output reg [15:0] UmbralV0_LOW_cond,
+    output reg [15:0] UmbralV1_HIGH_cond,
+    output reg [15:0] UmbralV1_LOW_cond,
+    //DF
+    output reg [3:0] UmbralD0_HIGH_cond,
+    output reg [3:0] UmbralD0_LOW_cond,
+    output reg [3:0] UmbralD1_HIGH_cond,
+    output reg [3:0] UmbralD1_LOW_cond,
     output reg [4:0] error_full_cond);
 
 
@@ -29,12 +43,21 @@ module maquina_estados_cond
 
 reg [2:0] estado;
 reg [2:0] estado_prox;
-reg [3:0] UmbralMF;
-reg [15:0] UmbralV0;
-reg [15:0] UmbralV1;
-reg [3:0] UmbralD0;
-reg [3:0] UmbralD1;
-parameter RESET = 0;
+//MF
+reg [3:0] UmbralMF_HIGH;
+reg [3:0] UmbralMF_LOW;
+//VF
+reg [15:0] UmbralV0_HIGH;
+reg [15:0] UmbralV0_LOW;
+reg [15:0] UmbralV1_HIGH;
+reg [15:0] UmbralV1_LOW;
+//DF
+reg [3:0] UmbralD0_HIGH;
+reg [3:0] UmbralD0_LOW;
+reg [3:0] UmbralD1_HIGH;
+reg [3:0] UmbralD1_LOW;
+//Estados
+parameter RESET_L = 0;
 parameter INIT = 1;
 parameter IDLE = 2;
 parameter ACTIVE = 3;
@@ -46,8 +69,8 @@ parameter ERROR = 4;
 
 
 always @(posedge clk) begin
-    if(reset==0) begin
-        estado <= RESET;
+    if(reset_L==0) begin
+        estado <= RESET_L;
     end
     else begin 
         estado<=estado_prox;
@@ -60,36 +83,59 @@ always @(*) begin
     active_out_cond=0;
     idle_out_cond=0;
     error_full_cond=0;
-    UmbralMF=UmbralesMFs;
-    UmbralV0=UmbralesVCs[31:16];
-    UmbralV1=UmbralesVCs[15:0];
-    UmbralD0=UmbralesDs[7:4];
-    UmbralD1=UmbralesDs[3:0];
-    if(reset==0)begin
-    UmbralMF_cond=0;
-    UmbralV0_cond=0;
-    UmbralV1_cond=0;
-    UmbralD0_cond=0;
-    UmbralD1_cond=0;
+    //MF
+    UmbralMF_HIGH=UmbralesMFs_HIGH;
+    UmbralMF_LOW=UmbralesMFs_LOW;
+    //VF
+    UmbralV0_HIGH=UmbralesVCs_HIGH[31:16];
+    UmbralV0_LOW=UmbralesVCs_LOW[31:16];
+    UmbralV1_HIGH=UmbralesVCs_HIGH[15:0];
+    UmbralV1_LOW=UmbralesVCs_LOW[15:0];
+    //DF
+    UmbralD0_HIGH=UmbralesDs_HIGH[7:4];
+    UmbralD0_LOW=UmbralesDs_LOW[7:4];
+    UmbralD1_HIGH=UmbralesDs_HIGH[3:0];
+    UmbralD1_LOW=UmbralesDs_LOW[3:0];
+    if(reset_L==0)begin
+    UmbralMF_HIGH_cond=0;
+    UmbralMF_LOW_cond=0;
+    UmbralV0_HIGH_cond=0;
+    UmbralV0_LOW_cond=0;
+    UmbralV1_HIGH_cond=0;
+    UmbralV1_LOW_cond=0;
+    UmbralD0_HIGH_cond=0;
+    UmbralD0_LOW_cond=0;
+    UmbralD1_HIGH_cond=0;
+    UmbralD1_LOW_cond=0;
     end else begin
-    UmbralMF_cond=UmbralMF;
-    UmbralV0_cond=UmbralV0;
-    UmbralV1_cond=UmbralV1;
-    UmbralD0_cond=UmbralD0;
-    UmbralD1_cond=UmbralD1;
+    UmbralMF_HIGH_cond=UmbralMF_HIGH;
+    UmbralMF_LOW_cond=UmbralMF_LOW;
+    UmbralV0_HIGH_cond=UmbralV0_HIGH;
+    UmbralV0_LOW_cond=UmbralV0_LOW;
+    UmbralV1_HIGH_cond=UmbralV1_HIGH;
+    UmbralV1_LOW_cond=UmbralV1_LOW;
+    UmbralD0_HIGH_cond=UmbralD0_HIGH;
+    UmbralD0_LOW_cond=UmbralD0_LOW;
+    UmbralD1_HIGH_cond=UmbralD1_HIGH;
+    UmbralD1_LOW_cond=UmbralD1_LOW;
     end
     case (estado)
-        RESET: begin
+        RESET_L: begin
             active_out_cond=0;
             error_out_cond=0;
             idle_out_cond=0;
             error_full_cond=0;
-            UmbralMF_cond=0;
-            UmbralV0_cond=0;
-            UmbralV1_cond=0;
-            UmbralD0_cond=0;
-            UmbralD1_cond=0;
-            if(reset==0)begin
+            UmbralMF_HIGH_cond=0;
+            UmbralMF_LOW_cond=0;
+            UmbralV0_HIGH_cond=0;
+            UmbralV0_LOW_cond=0;
+            UmbralV1_HIGH_cond=0;
+            UmbralV1_LOW_cond=0;
+            UmbralD0_HIGH_cond=0;
+            UmbralD0_LOW_cond=0;
+            UmbralD1_HIGH_cond=0;
+            UmbralD1_LOW_cond=0;
+            if(reset_L==0)begin
                 if(init==1)
                 estado_prox=INIT;
             end else
@@ -100,13 +146,18 @@ always @(*) begin
             idle_out_cond=0;
             active_out_cond=0;
             error_full_cond=0;
-            UmbralMF=UmbralesMFs;
-            UmbralV0=UmbralesVCs[31:16];
-            UmbralV1=UmbralesVCs[15:0];
-            UmbralD0=UmbralesDs[7:4];
-            UmbralD1=UmbralesDs[3:0];
-            if(reset==0)
-                estado_prox=RESET;
+            UmbralMF_HIGH=UmbralesMFs_HIGH;
+            UmbralMF_LOW=UmbralesMFs_LOW;
+            UmbralV0_HIGH=UmbralesVCs_HIGH[31:16];
+            UmbralV0_LOW=UmbralesVCs_LOW[31:16];
+            UmbralV1_HIGH=UmbralesVCs_HIGH[15:0];
+            UmbralV1_LOW=UmbralesVCs_LOW[15:0];
+            UmbralD0_HIGH=UmbralesDs_HIGH[7:4];
+            UmbralD0_LOW=UmbralesDs_LOW[7:4];
+            UmbralD1_HIGH=UmbralesDs_HIGH[3:0];
+            UmbralD1_LOW=UmbralesDs_LOW[3:0];
+            if(reset_L==0)
+                estado_prox=RESET_L;
             else if(init==1)
                 estado_prox=INIT;
             else if(FIFO_ERRORS!=4'b0)
@@ -121,8 +172,8 @@ always @(*) begin
             idle_out_cond=1;
             active_out_cond=0;
             error_full_cond=0;
-            if(reset==0)
-                estado_prox=RESET;
+            if(reset_L==0)
+                estado_prox=RESET_L;
             else if(FIFO_ERRORS!=4'b0)
                 estado_prox=ERROR;
             else if(FIFO_EMPTIES!=4'b0)
@@ -136,8 +187,8 @@ always @(*) begin
             error_out_cond=1;
             active_out_cond=0;
             error_full_cond= FIFO_ERRORS;
-            if(reset==0)
-                estado_prox=RESET;
+            if(reset_L==0)
+                estado_prox=RESET_L;
             else 
                 estado_prox=ERROR;
            
@@ -147,8 +198,8 @@ always @(*) begin
             error_out_cond=0;
             idle_out_cond=0;
             active_out_cond=1;
-            if(reset==0)
-                estado_prox=RESET;
+            if(reset_L==0)
+                estado_prox=RESET_L;
             else if(init==1)
                 estado_prox=INIT;
             else if(FIFO_ERRORS!=4'b0)
